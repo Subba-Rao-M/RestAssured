354513ff04d4bda9ec809adf782e46dd37c2fa07
Subba Rao <subba.rao@tietoevry.com>
2024-09-13 18:30:22 +0530

    Updated comments in a file

--- src/test/java/apiChaining/APIChainin.java
+++ src/test/java/apiChaining/APIChainin.java
@@ -5,7 +5,7 @@ public class APIChainin {
 	/**
 	 * API Chaining - Response data from one becomes request data in another request
 	 * testNg.xml file feature used here to set execution sequence
-	 * ITestContext used to set contexts
+	 * ITestContext used to set contexts and set variable values to available for other test cases
 	 * Priority can be used if all tests written in single class file and common variable can be declared which can be used by all testcases
 	 */
 	
